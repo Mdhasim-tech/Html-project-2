3b74bfb4529370699c1292bcdc683e9f37b9c221
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2023-08-10 16:00:45 +0530

    Add files via upload

---
+++ supreme education.js
@@ -0,0 +1,11 @@
+//change the card title to green
+// let ctitle=document.getElementsByClassName("card-title")[0]
+// ctitle.style.color="green"
+
+// let ctitle=document.getElementById("firstcardtitle")
+// ctitle.style.color="red"
+
+let ctitles=document.querySelectorAll(".card-title")
+ctitles[0].style.color="green"
+ctitles[1].style.color="green"
+ctitles[2].style.color="green"
